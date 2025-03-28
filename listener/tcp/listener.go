3ba1977a8f0c0fd99a96e39c7515df409c50afd7
zlb <zhoulbjava@163.com>
2025-03-28 17:14:16 +0800

    tcp关闭监听未释放连接fix

--- listener/tcp/listener.go
+++ listener/tcp/listener.go
@@ -3,6 +3,7 @@ package tcp
 import (
 	"context"
 	"net"
+	"sync"
 	"time"
 
 	"github.com/go-gost/core/limiter"
@@ -28,6 +29,12 @@ type tcpListener struct {
 	logger  logger.Logger
 	md      metadata
 	options listener.Options
+
+	connChan  chan net.Conn         // 用于接收新连接的channel
+	conns     map[net.Conn]struct{} // 用于存储所有连接
+	mu        sync.Mutex            // 保护 conns map 的互斥锁
+	closed    bool                  // 标记listener是否已经关闭
+	closeChan chan struct{}
 }
 
 func NewListener(opts ...listener.Option) listener.Listener {
@@ -36,8 +43,11 @@ func NewListener(opts ...listener.Option) listener.Listener {
 		opt(&options)
 	}
 	return &tcpListener{
-		logger:  options.Logger,
-		options: options,
+		logger:    options.Logger,
+		options:   options,
+		connChan:  make(chan net.Conn, 1024), // 可以调整channel的buffer大小
+		conns:     make(map[net.Conn]struct{}),
+		closeChan: make(chan struct{}),
 	}
 }
 
@@ -71,26 +81,71 @@ func (l *tcpListener) Init(md md.Metadata) (err error) {
 	ln = climiter.WrapListener(l.options.ConnLimiter, ln)
 	l.ln = ln
 
+	go l.acceptLoop() // 启动accept loop
+
 	return
 }
 
-func (l *tcpListener) Accept() (conn net.Conn, err error) {
-	conn, err = l.ln.Accept()
-	if err != nil {
-		return
+func (l *tcpListener) acceptLoop() {
+	for {
+		conn, err := l.ln.Accept()
+
+		if l.closed {
+			if conn != nil {
+				conn.Close()
+			}
+			return
+		}
+
+		if err != nil {
+			l.logger.Errorf("accept error: %v", err)
+			select {
+			case <-l.closeChan:
+				return
+			default:
+				// 如果没有关闭信号，则短暂休眠后继续尝试
+				time.Sleep(time.Millisecond * 100)
+				continue
+			}
+		}
+
+		select {
+		case l.connChan <- conn:
+		case <-l.closeChan:
+			if conn != nil {
+				conn.Close()
+			}
+			return
+		}
 	}
+}
 
-	conn = limiter_wrapper.WrapConn(
-		conn,
-		l.options.TrafficLimiter,
-		conn.RemoteAddr().String(),
-		limiter.ScopeOption(limiter.ScopeConn),
-		limiter.ServiceOption(l.options.Service),
-		limiter.NetworkOption(conn.LocalAddr().Network()),
-		limiter.SrcOption(conn.RemoteAddr().String()),
-	)
+func (l *tcpListener) Accept() (conn net.Conn, err error) {
+	select {
+	case conn := <-l.connChan:
+		l.mu.Lock()
+		if l.closed {
+			l.mu.Unlock()
+			conn.Close()
+			return nil, net.ErrClosed
+		}
+		l.conns[conn] = struct{}{}
+		l.mu.Unlock()
 
-	return
+		conn = limiter_wrapper.WrapConn(
+			conn,
+			l.options.TrafficLimiter,
+			conn.RemoteAddr().String(),
+			limiter.ScopeOption(limiter.ScopeConn),
+			limiter.ServiceOption(l.options.Service),
+			limiter.NetworkOption(conn.LocalAddr().Network()),
+			limiter.SrcOption(conn.RemoteAddr().String()),
+		)
+
+		return conn, nil
+	case <-l.closeChan:
+		return nil, net.ErrClosed
+	}
 }
 
 func (l *tcpListener) Addr() net.Addr {
@@ -98,5 +153,23 @@ func (l *tcpListener) Addr() net.Addr {
 }
 
 func (l *tcpListener) Close() error {
-	return l.ln.Close()
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	if l.closed {
+		return nil // 避免重复关闭
+	}
+
+	l.closed = true
+	close(l.closeChan) // Signal to stop accept loop
+
+	err := l.ln.Close()
+
+	// 关闭所有连接
+	for conn := range l.conns {
+		conn.Close()
+		delete(l.conns, conn)
+	}
+
+	return err
 }
